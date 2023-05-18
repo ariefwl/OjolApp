1d0ecfb388dbe2a8ed32ff3ff7330d5abc234391
ariefwl <ariefwl79@gmail.com>
2023-05-18 23:13:33 +0700

    Setting Navigation Page

---
+++ src/App.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import type {PropsWithChildren} from 'react';
+import {
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+
+import {NavigationContainer} from '@react-navigation/native';
+import Router from './router';
+
+type SectionProps = PropsWithChildren<{
+  title: string;
+}>;
+
+function App(): JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <NavigationContainer style={backgroundStyle}>
+      <Router />
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+});
+
+export default App;
